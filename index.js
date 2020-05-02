24ce737e8c4b7bb05edcea7c3bba44e01ce7fae9
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-05-02 09:15:37 -0700

    update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const inquirer = require('inquirer')
 const fs = require('fs')
 const axios = require('axios')
 const util = require('util')
-// const gm = require('./utils/generateMarkdown')
 const dedent = require('dedent')
 
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -11,7 +10,7 @@ inquirer.prompt([
     {
         type: 'input',
         message: 'What is your name?',
-        name: 'username',
+        name: 'name',
     },
     {
         type: 'input',
@@ -27,16 +26,39 @@ inquirer.prompt([
         type: 'input',
         message: 'What is the description of the project?',
         name: 'description',
-    }
+    },
+    {
+        type: 'list',
+        message: 'What is the primary coding language of the project?',
+        name: 'languages',
+        choices: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'NODE JS',
+            'React',
+            'MongoDB',
+            'MySQL'
+        ]
+    },
+    {
+        type: 'input',
+        message: 'Who helped contribute during this project?',
+        name: 'contributers',
+    },
+    {
+        type: 'input',
+        message: 'Please briefly describe the usage and purpose of the project?',
+        name: 'usage',
+    },
+
     //Creates file
 ])
     .then((data) => {
 
-        const { name, username, projectname, description } = data
-
+        const { name, username, projectname, description, languages, contributers, usage } = data
 
         const queryUrl = `https://api.github.com/users/${username}`;
-        const repoUrl = `https://api.github.com/users/${username}/repos?per_page=100`
 
         axios.get(queryUrl)
             .then((res) => {
@@ -46,7 +68,7 @@ inquirer.prompt([
 
                 axios.get(repos)
                     .then((repos) => {
-                        // const returnednames = [];
+
                         const repoNames = repos.data.map(function (repo) {
                             const returnednames = [repo.name, repo.html_url, repo.description, repo.language];
 
@@ -64,30 +86,51 @@ inquirer.prompt([
                         // console.log(repos)
 
                         const mdFile = dedent(`
-                            #${projectname}
+                        
+                            [![](https://img.shields.io/apm/MIT)](https://${username}.github.io/${projectname}/.)
+                            [![](https://img.shields.io/github/pipenv/locked/python-version/${username}/${projectname}?style=plastic)](https://${username}.github.io/${projectname}/.)
+                            [![](https://img.shields.io/visual-studio-app-center/releases/size/${username}/${projectname}/null)](https://${username}.github.io/${projectname}/.)
                             
-                            ##Description
                             
-                            ${description}
-                            
-                            ##Table of Contents
-                            * [Technologies Used](#technologies)
-                            *[Installation](#installation)
-                            *[Usage](#usage)
-                            *[Credits](#credits)
-                            *[Badges](#badges)
-                            *[Contributing](#contributing)
-                            *[Tests](#tests)
-                            *[License](#license)
 
-                            ##Overview
-                            # This project was created, reviewed, and deployed by ${name} **Github link: ${accountUrl}**
-                            !['alt text'](${imageUrl})
+
+                            # ${projectname}   
+                            ### ${description}
+                            
+                            # Table of Contents
+                            * [Primary Technology](#tecnology)
+                            * [Installation](#installation)
+                            * [Usage](#usage)
+                            * [Contributing](#contributing)
+                            
+                            ### [Primary Technology](#technology)
+                            * ${languages}
+
+                            ### [Installation](#installation)
+                            * Please follow GitHub link above to clone or fork repository. To sample the complete project, please click [here](https://${username}.github.io/${projectname}/.). To browse the source code of the project, please click [here](https://github.com/${username}/${projectname})
+
+                            ### [Usage](#usage)
+                            * This application was created and designed solely for purposes of learning, applying, and deploying mock applications for educational purposes. Please do not infringe or plagerize the code provided below.
+                            * ${usage}
+
+                            ### [Contributers](#contributers)
+                            * The following helped contribute to this project: ${contributers}
+
+
+                            # Author Overview
+                            ## This project was created, reviewed, and deployed by ${name} 
+
+                            !['profile picture'](${imageUrl})
+
+                                                        
+                            **Github link: ${accountUrl}**
+
+
                             # Past repos 
-                            **${repoNameStr}
-                            **${repoUrlStr}
-                            **${repoDescriptionStr}
-                            **${repoLanguageStr}
+                            ### ${repoNameStr}__
+                            ### ${repoUrlStr}__
+                            ### ${repoDescriptionStr}__
+
                             
                             `);
 
@@ -102,9 +145,6 @@ inquirer.prompt([
         if (err) throw err
         console.log('success!')
     })
-
-
-                // console.log(res)
 
 
 
